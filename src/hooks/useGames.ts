72c9c4d86cf6010781584e2834c43271a261e8d5
vatsac <avats218@gmail.com>
2024-05-01 17:46:20 +0530

    Displaying Platform Icons.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: {platform: Platform}[]
 }
 
 interface FetchGamesResponse {
@@ -32,82 +39,125 @@ const useGames = () => {
           ...games,
           {
             id: 1,
-            name: "Grand Theft Auto V",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?1",
-          },
-          {
+            name: 'Grand Theft Auto V',
+            background_image: 'https://source.unsplash.com/collection/190727/800x600?1',
+            parent_platforms: [
+                { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } },
+                { platform: { id: 2, name: 'Xbox', slug: 'xbox' } },
+                { platform: { id: 3, name: 'PC', slug: 'pc' } }
+            ]
+        },
+        {
             id: 2,
-            name: "The Witcher 3: Wild Hunt",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?2",
-          },
-          {
+            name: 'The Witcher 3: Wild Hunt',
+            background_image: 'https://source.unsplash.com/collection/190727/800x600?2',
+            parent_platforms: [
+                { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } },
+                { platform: { id: 2, name: 'Xbox', slug: 'xbox' } },
+                { platform: { id: 3, name: 'PC', slug: 'pc' } }
+            ]
+        },
+        {
             id: 3,
-            name: "Portal 2",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?3",
-          },
-          {
-            id: 4,
-            name: "Red Dead Redemption 2",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?4",
-          },
-          {
-            id: 5,
-            name: "The Last of Us Part II",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?5",
-          },
-          {
-            id: 6,
-            name: "God of War",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?6",
-          },
-          {
-            id: 7,
-            name: "Horizon Zero Dawn",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?7",
-          },
-          {
-            id: 8,
-            name: "Assassin's Creed Valhalla",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?8",
-          },
-          {
-            id: 9,
-            name: "Final Fantasy VII Remake",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?9",
-          },
-          {
-            id: 10,
-            name: "Minecraft",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?10",
-          },
-          {
-            id: 11,
-            name: "Call of Duty: Modern Warfare",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?11",
-          },
-          {
-            id: 12,
-            name: "The Elder Scrolls V: Skyrim",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?12",
-          },
-          {
-            id: 13,
-            name: "Cyberpunk 2077",
-            background_image:
-              "https://source.unsplash.com/collection/190727/800x600?13",
-          },
+            name: 'Portal 2',
+            background_image: 'https://source.unsplash.com/collection/190727/800x600?3',
+            parent_platforms: [
+                { platform: { id: 3, name: 'PC', slug: 'pc' } }
+            ]
+        },
+        {
+          id: 4,
+          name: 'Red Dead Redemption 2',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?4',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } },
+              { platform: { id: 2, name: 'Xbox', slug: 'xbox' } },
+              { platform: { id: 3, name: 'PC', slug: 'pc' } }
+          ]
+      },
+      {
+          id: 5,
+          name: 'The Last of Us Part II',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?5',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } }
+          ]
+      },
+      {
+          id: 6,
+          name: 'God of War',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?6',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } }
+          ]
+      },
+      {
+          id: 7,
+          name: 'Horizon Zero Dawn',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?7',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } }
+          ]
+      },
+      {
+          id: 8,
+          name: 'Assassin\'s Creed Valhalla',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?8',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } },
+              { platform: { id: 2, name: 'Xbox', slug: 'xbox' } },
+              { platform: { id: 3, name: 'PC', slug: 'pc' } }
+          ]
+      },
+      {
+          id: 9,
+          name: 'Final Fantasy VII Remake',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?9',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } }
+          ]
+      },
+      {
+          id: 10,
+          name: 'Minecraft',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?10',
+          parent_platforms: [
+              { platform: { id: 3, name: 'PC', slug: 'pc' } },
+              { platform: { id: 4, name: 'Nintendo', slug: 'nintendo' } },
+              { platform: { id: 5, name: 'Xbox', slug: 'xbox' } },
+              { platform: { id: 6, name: 'PlayStation', slug: 'playstation' } }
+          ]
+      },
+      {
+          id: 11,
+          name: 'Call of Duty: Modern Warfare',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?11',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } },
+              { platform: { id: 2, name: 'Xbox', slug: 'xbox' } },
+              { platform: { id: 3, name: 'PC', slug: 'pc' } }
+          ]
+      },
+      {
+          id: 12,
+          name: 'The Elder Scrolls V: Skyrim',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?12',
+          parent_platforms: [
+              { platform: { id: 3, name: 'PC', slug: 'pc' } },
+              { platform: { id: 6, name: 'PlayStation', slug: 'playstation' } },
+              { platform: { id: 5, name: 'Xbox', slug: 'xbox' } }
+          ]
+      },
+      {
+          id: 13,
+          name: 'Cyberpunk 2077',
+          background_image: 'https://source.unsplash.com/collection/190727/800x600?13',
+          parent_platforms: [
+              { platform: { id: 1, name: 'PlayStation', slug: 'playstation' } },
+              { platform: { id: 2, name: 'Xbox', slug: 'xbox' } },
+              { platform: { id: 3, name: 'PC', slug: 'pc' } }
+          ]
+      }
         ]);
       });
 
