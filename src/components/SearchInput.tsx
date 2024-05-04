57f8e2ecf3ccb748b3cf720ce0cef3c776de34e1
avats218@gmail.com <avats218@gmail.com>
2024-05-04 17:14:48 +0530

    Implement Searching.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,13 +1,31 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
-import { BsSearch } from "react-icons/bs"
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
-    return (
-        <InputGroup>
-        <InputLeftElement children={<BsSearch />} />
-     <Input borderRadius={20} placeholder='Search games...' variant='filled'/>
-     </InputGroup>
-    )
+interface Props {
+    onSearch: (searchText: string) => void;
 }
 
-export default SearchInput
+
+const SearchInput = ({onSearch}: Props) => {
+    const ref = useRef<HTMLInputElement>(null);
+
+  return (
+    <form onSubmit={(event)=> {
+        event.preventDefault();
+        if(ref.current) onSearch(ref.current.value);
+      }}>
+      <InputGroup >
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
+  );
+};
+
+export default SearchInput;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,7 +24,10 @@ const useGames = (
 ) =>
   useData<Game>(
     "/games",
-    { params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder } },
+    { params: { genres: gameQuery.genre?.id,
+       platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+         search: gameQuery.searchText } },
     [gameQuery]
   );
 
